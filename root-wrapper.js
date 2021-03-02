1d547f9678c99a306093e1edeb643a7f727cfd7a
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-02 21:40:55 +0000

    added code blocks

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,16 +1,38 @@
-import {MDXProvider} from '@mdx-js/react';
-import React from 'react';
+import { MDXProvider } from "@mdx-js/react";
+import React from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
 
-const components ={
-    h2: ({children}) => ( 
-        <h2 style={{color:'rebeccapurple'}}>{children}</h2>
-    ),
-    'p.inlineCode' : props => (
-        <code {...props} style={{backgroundColor: 'lightgrey'}}></code>
-    )
-     
-}
-export const wrapRootElement = ({element}) => 
-(<MDXProvider components={components}>
-    {element}
-</MDXProvider>)
+const components = {
+  h2: ({ children }) => <h2 style={{ color: "rebeccapurple" }}>{children}</h2>,
+  "p.inlineCode": (props) => (
+    <code {...props} style={{ backgroundColor: "lightgrey" }}></code>
+  ),
+  pre: (props) => {
+    const className = props.children.props.className || ``;
+    const matches = className.match(/language-(?<lang>.*)/);
+
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children.trim()}
+        language={matches && matches.groups && matches.groups.lang ? matches.groups.lang : '' }
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    );
+  },
+};
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+);
