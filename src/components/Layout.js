096078cf7b0cc0fce5a545be022e98f370a2401f
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-12 02:19:41 +0000

    changed blog width

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { useSiteMetadata } from '../hooks/useSiteMetadata';
 import { Header } from './Header';
 
 const AppStyles = styled.main`
-  width: 900px;
+  width: 80vw;
   margin: 0 auto;
 `;
 
