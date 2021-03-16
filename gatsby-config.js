0466c1cf5bc043bed41549843b50a9ad518761d9
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-16 18:54:32 +0000

    added gatsby image packages and added images to index

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,29 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
       },
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 80,
+          },
+        },
+      ],
+      plugin: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 80,
+          },
+        },
+      ],
     },
     {
       resolve: `gatsby-source-filesystem`,
