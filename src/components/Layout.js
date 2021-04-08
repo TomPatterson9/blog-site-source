7d00e63257500ce462c66b714427bdb8ed680b34
Tompatterson9 <thomas.patterson99@gmail.com>
2021-04-08 10:11:02 +0100

    changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import { useSiteMetadata } from '../hooks/useSiteMetadata';
 import { Header } from './Header';
 
+
 const AppStyles = styled.main`
   width: 80vw;
   margin: 0 auto;
+  font-family: Roboto, sans-serif, serif;
 `;
 
 export const Layout = ({ children }) => {
